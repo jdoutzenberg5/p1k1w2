f6f5723251a562eea402366f7223bdcfb379eb06
Gebruiker <Gebruiker@LAPTOP-7BP05JQ5>
2025-02-25 13:33:01 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace p1k1w2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siticoneButton1 = new SiticoneNetFrameworkUI.SiticoneButton();
+            this.SuspendLayout();
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.AccessibleDescription = "The default button control that accept input though the mouse, touch and keyboard" +
+    "";
+            this.siticoneButton1.AccessibleName = "Clear RecycleBin";
+            this.siticoneButton1.AutoSizeBasedOnText = false;
+            this.siticoneButton1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneButton1.BadgeBackColor = System.Drawing.Color.Red;
+            this.siticoneButton1.BadgeFont = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold);
+            this.siticoneButton1.BadgeValue = 0;
+            this.siticoneButton1.BadgeValueForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(15)))), ((int)(((byte)(130)))));
+            this.siticoneButton1.BorderWidth = 2;
+            this.siticoneButton1.ButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(15)))), ((int)(((byte)(130)))));
+            this.siticoneButton1.ButtonImage = null;
+            this.siticoneButton1.CanBeep = true;
+            this.siticoneButton1.CanGlow = false;
+            this.siticoneButton1.CanShake = true;
+            this.siticoneButton1.ContextMenuStripEx = null;
+            this.siticoneButton1.CornerRadiusBottomLeft = 0;
+            this.siticoneButton1.CornerRadiusBottomRight = 0;
+            this.siticoneButton1.CornerRadiusTopLeft = 0;
+            this.siticoneButton1.CornerRadiusTopRight = 0;
+            this.siticoneButton1.CustomCursor = System.Windows.Forms.Cursors.Default;
+            this.siticoneButton1.DisabledTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.siticoneButton1.EnableLongPress = false;
+            this.siticoneButton1.EnablePressAnimation = true;
+            this.siticoneButton1.EnableRippleEffect = true;
+            this.siticoneButton1.EnableShadow = false;
+            this.siticoneButton1.EnableTextWrapping = false;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.GlowColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.siticoneButton1.GlowIntensity = 100;
+            this.siticoneButton1.GlowRadius = 20F;
+            this.siticoneButton1.GradientBackground = false;
+            this.siticoneButton1.GradientColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(168)))), ((int)(((byte)(255)))));
+            this.siticoneButton1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.siticoneButton1.HintText = null;
+            this.siticoneButton1.HoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(168)))), ((int)(((byte)(255)))));
+            this.siticoneButton1.HoverFontStyle = System.Drawing.FontStyle.Regular;
+            this.siticoneButton1.HoverTextColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoverTransitionDuration = 250;
+            this.siticoneButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.siticoneButton1.ImagePadding = 5;
+            this.siticoneButton1.ImageSize = new System.Drawing.Size(16, 16);
+            this.siticoneButton1.IsRadial = false;
+            this.siticoneButton1.IsReadOnly = false;
+            this.siticoneButton1.IsToggleButton = false;
+            this.siticoneButton1.IsToggled = false;
+            this.siticoneButton1.Location = new System.Drawing.Point(175, 77);
+            this.siticoneButton1.LongPressDurationMS = 1000;
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.NormalFontStyle = System.Drawing.FontStyle.Regular;
+            this.siticoneButton1.ParticleColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.siticoneButton1.ParticleCount = 15;
+            this.siticoneButton1.PressAnimationScale = 0.97F;
+            this.siticoneButton1.PressedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(128)))), ((int)(((byte)(235)))));
+            this.siticoneButton1.PressedFontStyle = System.Drawing.FontStyle.Regular;
+            this.siticoneButton1.PressTransitionDuration = 150;
+            this.siticoneButton1.ReadOnlyTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.siticoneButton1.RippleColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.siticoneButton1.RippleOpacity = 0.3F;
+            this.siticoneButton1.RippleRadiusMultiplier = 0.6F;
+            this.siticoneButton1.ShadowBlur = 5;
+            this.siticoneButton1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.siticoneButton1.ShadowOffset = new System.Drawing.Point(2, 2);
+            this.siticoneButton1.ShakeDuration = 500;
+            this.siticoneButton1.ShakeIntensity = 5;
+            this.siticoneButton1.Size = new System.Drawing.Size(187, 56);
+            this.siticoneButton1.TabIndex = 0;
+            this.siticoneButton1.Text = "Clear RecycleBin";
+            this.siticoneButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.siticoneButton1.TextColor = System.Drawing.Color.White;
+            this.siticoneButton1.TooltipText = null;
+            this.siticoneButton1.UseAdvancedRendering = true;
+            this.siticoneButton1.UseParticles = false;
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.siticoneButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SiticoneNetFrameworkUI.SiticoneButton siticoneButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace p1k1w2
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "powershell.exe";
+            processStartInfo.Arguments = "Clear-RecycleBin -Force Start-Sleep \r\n-Seconds 2 \r\nClear-RecycleBin -Force";
+            processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(processStartInfo);
+            await Task.Delay(2000);
+            MessageBox.Show("Recycle Bin has been cleared.");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
